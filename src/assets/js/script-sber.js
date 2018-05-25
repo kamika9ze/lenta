b6d62a0cf0cb87576befd50df6b6bcad8d551d75
kamika9ze <kamika9ze@gmail.com>
2018-05-26 00:11:48 +0600

    add new desine

--- src/assets/js/script-sber.js
+++ src/assets/js/script-sber.js
@@ -2,3 +2,7 @@ $(".section-history-btn").click(function () {
     $(this).parents('.section-history').find('.section-history-hide-text').show();
     $(this).hide();
 });
+$(".btn-top-section-history").click(function () {
+    $(this).parents('.section-history').find('.section-history-hide-text').hide();
+    $(this).parents('.section-history').find('.section-history-btn').show();
+});
